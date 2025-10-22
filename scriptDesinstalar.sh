cae358947097b66be54816bad0de237458568b8c
HectorCazares-10 <l22050655@saltillo.tecnm.mx>
2025-10-22 08:05:30 -0600

    Agregar script de desinstalacion

---
+++ scriptDesinstalar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Desinstalar proyecto"
+
+# Deterner los procesos Node.js 
+pkill -f "node"
+pkill -f "vite"
+
+# Eliminar los node_modules de las carpetas
+rm -rf CN_api_bk/node_modules
+rm -rf CN_React/node_modules
+
+# Eliminar base de datos y usuarios
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS Proyecto;"
+sudo -u postgres psql -c "DROP USER IF EXISTS usuario1"
+echo "Base de datos eliminado"
+
+echo "Desinstalar Node.js y PostgreSQL"
+sudo apt remove --purge -y nodejs npm postgresql postgresql-contrib
+sudo apt autoremove -y
+
+echo "Desinstalacion terminada"
