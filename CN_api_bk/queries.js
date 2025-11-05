da23188569aa4d3a85fadff08ff2fad1fb465048
HectorCazares-10 <l22050655@saltillo.tecnm.mx>
2025-11-05 09:17:44 -0600

    Crear dockerfile y dockerfile compose, modificar queries.js

--- CN_api_bk/queries.js
+++ CN_api_bk/queries.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 var fs = require('fs');
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'database_bk',
-    password: 'postgres',
-    port: 5432,
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'database_bk',
+    password: process.env.DB_PASSWORD || 'postgres',
+    port: process.env.DB_PORT || 5432,
     ssl: false
 })
 
