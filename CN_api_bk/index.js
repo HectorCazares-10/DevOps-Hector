5d62212307d6680c40d34175819d853fa4ba9d3d
HectorCazares-10 <l22050655@saltillo.tecnm.mx>
2025-12-16 12:35:08 -0600

    cambiar puerto

--- CN_api_bk/index.js
+++ CN_api_bk/index.js
@@ -30,7 +30,7 @@ app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`APP running on port ${port}.`)
 })
 
