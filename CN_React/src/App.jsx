725a6d186a4cff901afff2214b1f807923538362
HectorCazares-10 <l22050655@saltillo.tecnm.mx>
2025-12-16 17:29:25 -0600

    Modificar archivos y agregar configuracion

--- CN_React/src/App.jsx
+++ CN_React/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState, useEffect } from 'react';
-import './App.css'
+import { useEffect, useState } from 'react';
+import { API_URL } from './config';
 
 function App() {
-  const [data, setData] = useState(null);
-  useEffect(()=> {
-    fetch("http://localhost:3000/users",{
-      'mode': 'cors',
-      'headers': {
-          'Access-Control-Allow-Origin': '*',
-      }
-    })
-      .then((response) => response.json())
-      .then((data) => setData(data));
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch(`${API_URL}/users`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Error en backend');
+        }
+        return res.json();
+      })
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
   }, []);
+
   return (
-    <div className='App'>
-      <h1>Fetch</h1>
-      <div className='card'>
-        <ul>
-          {data?.map((user)=>(
-            <li key={user.id}>{user.name}</li>
-
-          ))}
-        </ul>
-      </div>
-
+    <div>
+      <h1>Usuarios</h1>
+      <ul>
+        {users.map(u => (
+          <li key={u.id}>
+            {u.name} - {u.email}
+          </li>
+        ))}
+      </ul>
     </div>
-
   );
 }
 
-export default App
+export default App;
+
